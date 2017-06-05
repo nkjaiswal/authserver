fe76fe42c6a3af5f385d8e99df4ea7839df11585
Nishant Kumar <nishant.kumar03@sap.com>
2017-06-05 18:17:42 +0530

    Ui update

--- admin/controller/Main.controller.js
+++ admin/controller/Main.controller.js
@@ -4,6 +4,36 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("idpadminIDPAdmin.controller.Main", {
-
+		getData: function(path, successCallback, errorCallback) {
+			if (path === "UserData") {
+				var obj = {};
+				obj.userid = "I322345";
+				obj.userName = "Nishant";
+				obj.emailid = "nishant.soft04@gmail.com";
+				successCallback(obj);
+			}
+		},
+		onInit: function() {
+			var that = this;
+			this.getData("UserData", function(data) {
+				var oModel = new sap.ui.model.json.JSONModel();
+				oModel.setData(data);
+				that.getView().setModel(oModel, "UserData");
+			});
+		},
+		handleUserItemPressed: function(oEvent) {
+			var oButton = oEvent.getSource();
+			if (!this._actionSheet) {
+				this._actionSheet = sap.ui.xmlfragment(
+					"idpadminIDPAdmin.fragment.Logout",
+					this
+				);
+				this.getView().addDependent(this._actionSheet);
+			}
+			this._actionSheet.openBy(oButton);
+		},
+		handleLogout : function(){
+			window.loccation.href = "/admin/logout";
+		}
 	});
 });
